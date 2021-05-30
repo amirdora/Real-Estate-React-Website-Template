11e08dc34e9162bdb918f4990bfabd54b5cff436
Amir Dora <amirdora@mail.uni-paderborn.de>
2021-05-30 16:58:54 +0200

    smart react dom update vs js

---
+++ script.js
@@ -0,0 +1,29 @@
+const jsDiv = document.getElementById("dateJs")
+const reactDiv = document.getElementById("dateReact")
+
+const render = () => {
+    jsDiv.innerHTML = `
+    <div class="split">
+    Js Template
+    <input/>
+    <p> ${new Date()}</p>
+    </div>
+    `;
+
+    var divToRender = React.createElement(
+        "div",
+        { className: "split" },
+        "React template",
+        React.createElement("input"),
+        React.createElement(
+            "p",
+            null,
+            new Date().toString()
+        )
+    );
+    ReactDOM.render(divToRender, reactDiv);
+}
+
+
+
+setInterval(render, 1000)
